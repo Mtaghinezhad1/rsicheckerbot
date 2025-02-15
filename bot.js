b5b4d5520be8bf79c70c2e8ac71bc6eb893086e4
Mtaghinezhad1 <116306240+Mtaghinezhad1@users.noreply.github.com>
2025-02-15 03:54:40 +0330

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,20 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+// توکن ربات خود را از BotFather دریافت کنید و جایگزین کنید
+const token = '8159631062:AAEyUWtQYMb5YR_zTWvgUkOO9oFCVkrhfVw';
+
+// ایجاد یک نمونه از ربات
+const bot = new TelegramBot(token, { polling: true });
+
+// هنگامی که کاربر پیامی ارسال می‌کند
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+  const text = msg.text;
+
+  // اگر کاربر پیام "/start" را ارسال کند
+  if (text === '/start') {
+    bot.sendMessage(chatId, 'Hello World!');
+  }
+});
+
+console.log('bot is running');
